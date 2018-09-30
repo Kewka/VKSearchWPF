3c19babb1cbf19afe88f1d87f2e6fc87ac671016
Nikita Korshunov <kewka96@gmail.com>
2018-10-01 03:38:53 +0500

    Add User model

---
+++ VkSearchWPF/Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace VkSearchWPF.Models
+{
+    public class UserModel
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+        [JsonProperty("photo_max")]
+        public string PhotoUrl { get; set; }
+        [JsonIgnore]
+        public string FullName => FirstName + " " + LastName;
+    }
+}
