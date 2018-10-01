b1e9b704de83452549cab030aebbea9c485e28d9
Nikita Korshunov <kewka96@gmail.com>
2018-10-01 21:23:13 +0500

    Add Search Page View and Base View Model

---
+++ VkSearchWPF/ViewModels/BaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace VkSearchWPF.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool SetProperty<T>(ref T field, T value, [CallerMemberName]string property = null)
+        {
+            if (Equals(field, value))
+            {
+                return false;
+            }
+            field = value;
+            RaisePropertyChanged(property);
+            return true;
+        }
+
+        private void RaisePropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
--- VkSearchWPF/Views/SearchPage.xaml.cs
+++ VkSearchWPF/Views/SearchPage.xaml.cs
@@ -12,19 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using VkSearchWPF.Views;
 
-namespace VkSearchWPF
+namespace VkSearchWPF.Views
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для SearchPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SearchPage : Page
     {
-        public MainWindow()
+        public SearchPage()
         {
             InitializeComponent();
-            this.NavigationFrame.Navigate(new SearchPage());
         }
     }
 }
